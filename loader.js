e3b4c03d5351005ea0aad7138e22180643280286
nizameddinalyaprak <nizameddin.alyaprak@hacettepe.edu.tr>
2026-09-18 15:06:15 +0300

    fix: iletisim telefon numaralari dahili 136 ile iki satir halinde guncellendi

--- loader.js
+++ loader.js
@@ -1844,7 +1844,8 @@
                     Cankaya / ANKARA, TURKEY
                 </div>
                 <div class="hi-footer-contact">
-                    <a href="tel:+903123116015"><i class="fas fa-phone"></i> +90 (312) 311 60 15</a>
+                    <a href="tel:+903123116015"><i class="fas fa-phone"></i> +90 (312) 311 60 15 - 136</a>
+                    <a href="tel:+903122976983"><i class="fas fa-phone"></i> +90 (312) 297 69 83 - 136</a>
                 </div>
             </div>
             <div class="hi-footer-col">
@@ -1898,7 +1899,8 @@
                     Çankaya / ANKARA
                 </div>
                 <div class="hi-footer-contact">
-                    <a href="tel:+903123116015"><i class="fas fa-phone"></i> +90 (312) 311 60 15</a>
+                    <a href="tel:+903123116015"><i class="fas fa-phone"></i> +90 (312) 311 60 15 - 136</a>
+                    <a href="tel:+903122976983"><i class="fas fa-phone"></i> +90 (312) 297 69 83 - 136</a>
                 </div>
             </div>
             <div class="hi-footer-col">
